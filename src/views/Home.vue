6bdf1fdef2a02f7d3ef35160380e82cbea469d43
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-09 22:33:53 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="home">
+    <van-button type="primary">主要按钮</van-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div style="width:50px">421412</div>
+    <ul>
+      <li>421412</li>
+      <li>421412</li>
+      <li>421412</li>
+      <li>421412</li>
+    </ul>
   </div>
 </template>
 
