1f50a53d562c8f0f910bba1f115f8a87c7a3a834
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-09 23:00:52 +0800

    update css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
 
-import "./style/base.less";
+import "@/style/base.less";
 // 引入vant组件
-import "./plugin/vant";
+import "@/plugin/vant";
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,49 @@
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const isProd = process.env.NODE_ENV === 'production'
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     if (isProd) {
       // 配置webpack 压缩
       config.plugins.push(
         new CompressionWebpackPlugin({
           test: /\.js$|\.html$|\.css$/,
           // 超过4kb压缩
-          threshold: 4096
+          threshold: 4096,
         })
-      )
+      );
     }
-  }
-}
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
+        lessOptions: {
+          modifyVars: {
+            hack: `true; @import "~@/style/vant-ui.less";`,
+          },
+        },
+      },
+    },
+  },
+  // TODO 不太懂css
+  // css: {
+  //   // 是否将css 提取到独立的文件,生产环境提取，开发环境不提取
+  //   extract: !!isProd,
+  //   // 开发模式开启css sourcemap
+  //   sourceMap: !isProd,
+  //   loaderOptions: {
+  //     less: {
+  //       lessOptions: {
+  //         modifyVars: {
+  //           hack: 'true;@import "~@/style/_variables.less"',
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
+  devServer: {
+    port: 9001,
+    open: true,
+  },
+};
