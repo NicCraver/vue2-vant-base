79cc44c828d73746402c18027aa0459f9bfcd005
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-09 23:24:01 +0800

    update README.md

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
-
+// 引入style
 import "@/style/base.less";
+// 注册全局组件
+import CustomComponent from "./components";
 // 引入vant组件
 import "@/plugins/vant";
+
+Vue.use(CustomComponent, {});
 
 Vue.config.productionTip = false;
 
