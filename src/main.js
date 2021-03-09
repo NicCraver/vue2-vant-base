7e4d1f945a68045425afdafd0cfa8f34be399af0
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-09 22:32:24 +0800

    add style

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import "./style/base.less";
+// 引入vant组件
+import "./plugin/vant";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
