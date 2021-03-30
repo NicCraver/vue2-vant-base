dd2c832552bf09aef9e7e4b91436ecfeb138956a
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-30 17:59:57 +0800

    update mock

--- mock/modules/demo.js
+++ mock/modules/demo.js
@@ -1,13 +1,13 @@
 const Mock = require("mockjs");
 
-const userList = {
-  url: "/api/list",
-  type: "post",
+const getList = {
+  url: "/api/getList",
+  type: "get",
   response: () => {
     return {
       code: 20000,
       data: Mock.mock({
-        "books|1-10": [
+        "user|30-50": [
           {
             "sid|+1": 1,
             date: '@date("yyyy-MM-dd")',
@@ -20,5 +20,36 @@ const userList = {
     };
   }
 };
+const postList = {
+  url: "/api/postList",
+  type: "post",
+  response: () => {
+    return {
+      code: 20000,
+      data: Mock.mock({
+        "list|50-100": [
+          {
+            "sid|+1": 1,
+            date: '@date("yyyy-MM-dd")',
+            name: "@csentence()",
+            address: "@county(true)",
+            region: "双流区"
+          }
+        ]
+      })
+    };
+  }
+};
+const loginFail = {
+  url: "/api/loginFail",
+  type: "post",
+  response: () => {
+    return {
+      code: 403,
+      data: {},
+      msg: "账号或密码错误"
+    };
+  }
+};
 
-module.exports = [userList];
+module.exports = [getList, postList, loginFail];
--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+
 // 引入style
 import "@/style/base.less";
-// 注册全局组件
+
+// todo注册全局组件
 import CustomComponent from "./components";
+
 // 引入vant组件
 import "@/plugins/vant";
-require("../mock");
+
+// VUE_APP_MOCK的值为true，并且在开发环境下使用mock
+if (process.env.VUE_APP_MOCK && process.env.NODE_ENV === "development") {
+  require("../mock");
+}
+
 Vue.use(CustomComponent, {});
 
 Vue.config.productionTip = false;
