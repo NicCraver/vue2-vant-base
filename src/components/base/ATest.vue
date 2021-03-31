4f63355d84004ad4be7d75d4187fa640f72b694a
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-03-31 13:58:50 +0800

    feat：base components

---
+++ src/components/base/ATest.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>ATest</div>
+</template>
+
+<script>
+export default {
+  name: "ATest"
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
 // 扫描所有的组件
-const files = require.context('./', true, /^\.\/[\w-_]+\/index\.(js|vue)$/)
+// 组件名称必须首字母大写
+// 组件内部必须有name属性
+const files = require.context("./", true, /[A-Z]\w+\.(vue|js)$/);
 
 const components = files.keys().map(key => {
-  return files(key).default
-})
+  return files(key).default;
+});
 
 export default {
   install(Vue) {
     components.forEach(component => {
-      Vue.component(component.name, component)
-    })
+      Vue.component(component.name, component);
+    });
   }
-}
+};
