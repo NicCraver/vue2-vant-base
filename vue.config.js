ffab2aaec1cdd279088468b2eb8d76149bfba834
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-04-01 11:40:20 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -14,34 +14,6 @@ module.exports = {
       );
     }
   },
-  css: {
-    loaderOptions: {
-      less: {
-        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
-        lessOptions: {
-          modifyVars: {
-            hack: `true; @import "~@/style/vant-ui.less";`
-          }
-        }
-      }
-    }
-  },
-  // TODO 不太懂css
-  // css: {
-  //   // 是否将css 提取到独立的文件,生产环境提取，开发环境不提取
-  //   extract: !!isProd,
-  //   // 开发模式开启css sourcemap
-  //   sourceMap: !isProd,
-  //   loaderOptions: {
-  //     less: {
-  //       lessOptions: {
-  //         modifyVars: {
-  //           hack: 'true;@import "~@/style/_variables.less"',
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
   devServer: {
     port: 9001,
     open: true
