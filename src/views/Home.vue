b1fc6bba870b9678f0edf612a00ed60860c8863c
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-04-02 17:00:09 +0800

    feat：MallApp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,7 +50,11 @@
           is-link
           @click="GoToPage('/Examples/Decorators')"
         />
-        <van-cell title="Css Mixin" is-link @click="GoToPage('/Examples/CssMixin')" />
+        <van-cell
+          title="Css Mixin"
+          is-link
+          @click="GoToPage('/Examples/CssMixin')"
+        />
         <van-cell title="404" is-link @click="GoToPage('123456789')" />
         <van-cell title="Vuex" is-link @click="GoToPage('/Examples/Vuex')" />
         <van-cell title="Vuex" is-link @click="GoToPage('/Examples/Vuex')" />
