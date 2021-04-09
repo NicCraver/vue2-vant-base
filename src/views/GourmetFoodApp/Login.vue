1072bcae3d1a1d9eb8fff27d6140572f31f7abba
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-04-09 17:07:14 +0800

    Update Login.vue

--- src/views/GourmetFoodApp/Login.vue
+++ src/views/GourmetFoodApp/Login.vue
@@ -1,33 +1,34 @@
 <template>
   <div class="login">
     <header class="login_header">
-      Sign In
+      {{ pageTitle }}
     </header>
     <main class="login_main">
       <van-form @submit="onSubmit">
         <van-field
           v-model="username"
-          name="用户名"
-          label="用户名"
-          placeholder="用户名"
+          name="Email"
+          label="Email"
           :rules="[{ required: true, message: '请填写用户名' }]"
         />
         <van-field
           v-model="password"
           type="password"
-          name="密码"
-          label="密码"
-          placeholder="密码"
+          name="Password"
+          label="Password"
           :rules="[{ required: true, message: '请填写密码' }]"
         />
-        <div style="margin: 16px;">
+        <div class="btn">
           <van-button round block type="info" native-type="submit">
-            提交
+            {{ pageTitle }}
           </van-button>
         </div>
       </van-form>
     </main>
-    <footer></footer>
+    <footer class="login_footer">
+      <span>Already have an account?</span>
+      <span class="m-l-20">Sign In</span>
+    </footer>
   </div>
 </template>
 
@@ -35,9 +36,16 @@
 export default {
   data() {
     return {
+      // 0 登录 1 注册
+      pageStatus: 0,
       username: "",
       password: ""
     };
+  },
+  computed: {
+    pageTitle() {
+      return this.pageStatus === 0 ? "Sign In" : "Sign Up";
+    }
   },
   methods: {
     onSubmit(values) {
@@ -54,6 +62,7 @@ export default {
   background: url("../../assets/FoodExample/Bitmap@2x.png") no-repeat;
   background-size: 100%;
   box-sizing: border-box;
+  position: relative;
   .login_header {
     padding-top: 120px;
     padding-left: 19px;
@@ -67,12 +76,50 @@ export default {
   }
   .login_main {
     width: 90%;
+    box-sizing: border-box;
     margin-top: 114px;
-    height: 223px;
+    height: 160px;
     opacity: 1;
     background: #ffffff;
     border-radius: 0 22px 22px 0;
-    // background: #fff;
+    padding: 20px 20px 20px 0;
+    .btn {
+      margin-top: 30px;
+      width: 88%;
+      .van-button {
+        border-radius: 0 25px 25px 0;
+      }
+      .van-button--info {
+        color: #fff;
+        background-color: #fa7268;
+        border: 1px solid #fa7268;
+      }
+      .van-button__content {
+        display: flex;
+        align-items: center;
+        justify-content: left;
+        font-size: 17px;
+        font-family: SFProText-Semibold;
+        text-align: left;
+        color: #ffffff;
+        line-height: 22px;
+        letter-spacing: 0px;
+      }
+    }
   }
+  .login_footer {
+    width: 100vw;
+    position: absolute;
+    bottom: 20px;
+    color: #ffffff;
+    text-align: center;
+    font-size: 13px;
+    font-family: SFProText-Medium;
+    color: #ffffff;
+    line-height: 18px;
+  }
+}
+.m-l-20 {
+  margin-left: 20px;
 }
 </style>
