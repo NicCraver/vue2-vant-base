58a758df1b063d11de535c547097545799b76d06
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-04-13 10:39:16 +0800

    Update GourmetFood.js

--- src/router/modules/GourmetFood.js
+++ src/router/modules/GourmetFood.js
@@ -4,5 +4,11 @@ export default [
     name: "Login",
     component: () => import("@/views/GourmetFoodApp/Login.vue"),
     meta: { title: "Login" }
-  }
+  },
+  {
+    path: "/GourmetFood/SignUp",
+    name: "SignUp",
+    component: () => import("@/views/GourmetFoodApp/SignUp.vue"),
+    meta: { title: "SignUp" }
+  },
 ];
