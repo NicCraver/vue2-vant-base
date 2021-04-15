3001e20b1337bbed4310cd9a28789fea1e76ed2a
Nic_ <36131101+NicCraver@users.noreply.github.com>
2021-04-15 17:02:07 +0800

    Update Home.vue

--- src/views/GourmetFoodApp/Home.vue
+++ src/views/GourmetFoodApp/Home.vue
@@ -10,6 +10,35 @@
     </header>
     <main class="home_main">
       <div class="title">Welcome</div>
+      <div class="sorts">
+        <div>
+          <div class="img">
+            <img src="@/assets/FoodExample/roast-chicken@2x.png" alt="" />
+          </div>
+          <div class="text">Chicken</div>
+        </div>
+        <div>
+          <div class="img">
+            <img src="@/assets/FoodExample/steak@2x.png" alt="" />
+          </div>
+          <div class="text">Steak</div>
+        </div>
+        <div>
+          <div class="img">
+            <img src="@/assets/FoodExample/takeaway (2)@2x.png" alt="" />
+          </div>
+          <div class="text">Kake</div>
+        </div>
+        <div>
+          <div class="img">
+            <img src="@/assets/FoodExample/rice@2x.png" alt="" />
+          </div>
+          <div class="text">Grilled</div>
+        </div>
+      </div>
+      <div class="signboard">
+
+      </div>
     </main>
   </div>
 </template>
@@ -49,11 +78,40 @@ export default {};
   }
   .home_main {
     .title {
+      padding: 10px 0 0 15px;
       color: #1b2749;
       font-size: 34px;
       font-family: SFProDisplay-Bold;
       text-align: left;
       letter-spacing: 0px;
+    }
+    .sorts {
+      display: flex;
+      align-items: center;
+      padding: 20px 20px 20px 10px;
+      box-sizing: border-box;
+      .text {
+        text-align: center;
+        margin-right: 30px;
+        margin-top: 5px;
+        font-size: 11px;
+        font-family: SFProText-Medium;
+        color: #1b2749;
+        line-height: 13px;
+      }
+      .img {
+        margin-right: 30px;
+        background: #efeff2;
+        border-radius: 50%;
+        width: 45px;
+        height: 45px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        img {
+          width: 23px;
+        }
+      }
     }
   }
 }
